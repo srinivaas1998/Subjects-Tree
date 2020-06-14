54226e58d4bb6b6d4dfad024447fd6a3901df87f
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2020-06-14 12:01:38 +0530

    Update subject.js

--- subject.js
+++ subject.js
@@ -1,8 +1,14 @@
+var html='<form name="form" onsubmit="execfunc()">resourse:<input type="text" name="resourse" id="resourse" >link:<input type="text" name="link" id="link"><input type="submit" name="submit" class="submit"></form>';
+
+var res=[]
+var subject = function(resourse,link) {
+        this.resourse=resourse;
+        this.link = link;
+        
+};
 var toggler1 = document.getElementsByClassName("sub");
 var toggler2 = document.querySelectorAll('.sub1');
 var toggler3 = document.querySelectorAll('.sub2');
-
-
 
 var i;
 
@@ -24,21 +30,66 @@ for (i = 0; i < toggler3.length; i++) {
     
   });
 }
-function deleteSub(selectorID) {
-            
-            var el = document.getElementById(selectorID);
-            el.parentNode.removeChild(el);
-            document.getElementsByClassName('#selectorID').textContent="";
-            
+function deleteSub(el) {
+  el.parentNode.parentNode.removeChild(el.parentNode);
 };
-document.querySelector('.item__delete').addEventListener('click', function(event){
-	
 
-	var item=this;
-	var itemID=this.id;
-	deleteSub(itemID);
-	document.querySelector('.item__delete').removeChild(document.querySelector('.item__delete--btn'));
+document.querySelectorAll('.item__delete').forEach(el=>{
+  el.addEventListener('click', function(event){
 
-});
+  	var item=this;
+  	var itemID=this.id;
+  	deleteSub(this);
+  });
+})
+function additem(resourse,link){
+	newItem = new Subject(resourse,link);
+	res.push(newItem);
+	console.log(res);
+	return newItem;
+};
+function addtoUI(obj){
+	var element,html1,newHtml;
+	element='.sub2';
+	html1='<li><a href="%link%">%resourse%</a></li>';
+	newHtml = html.replace('%resourse%', obj.resourse);
+    newHtml = newHtml.replace('%link%', obj.link);
+    document.querySelector(element).innerHTML=newHtml;
+};
 
- 
+function getInput() {
+    return {
+        resourse: document.getElementById("resourse").value, // Will be either inc or exp
+        link: document.getElementById("link").value
+       
+    }
+};
+document.querySelectorAll('.item__add').forEach(el=>{
+	el.addEventListener('click',function(event){
+		var element;
+		element='.item__add';
+		
+		   
+        document.querySelector(element).innerHTML=html;
+        document.body.appendChild(form);
+
+		      
+       
+	});
+     
+
+
+})
+function execfunc(){
+
+	var input=getInput();
+	console.log(input);
+	if (input.resourse !== ""&& input.link !==""){ 
+		newItem = additem(input.resourse,input.link);
+      		addtoUI(newItem);
+
+
+	}
+
+};
+
